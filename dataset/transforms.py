c1584a1df2786847da902d96323e068c29e0af1a
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-05-10 15:32:37 +0100

    Update transforms.py

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -155,7 +155,7 @@ class Augmentation(object):
         video_clip_to_tensor=torch.stack(video_clip_to_tensor, dim=1)
         target = torch.as_tensor(target).float()
         uniform_temporal_subsample = UniformTemporalSubsample(num_samples=30)
-        print("****************type***********",type(video_clip_to_tensor))
+        
         video_clip = uniform_temporal_subsample(video_clip_to_tensor)
         
         return video_clip, target 
